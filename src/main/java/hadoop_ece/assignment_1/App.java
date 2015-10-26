e428aed1f985789596aed82261d45bf1ccdeff7f
Maxime Vidal <maximevidal@me.com>
2015-10-26 11:15:02 +0100

    First commit

---
+++ src/main/java/hadoop_ece/assignment_1/App.java
@@ -0,0 +1,13 @@
+package hadoop_ece.assignment_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
