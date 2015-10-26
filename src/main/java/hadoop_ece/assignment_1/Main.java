e46824b5786392a188c0b3aac1ba79626ce58bbb
Maxime Vidal <maximevidal@me.com>
2015-10-26 11:41:48 +0100

    Word count example in practice_1

---
+++ src/main/java/hadoop_ece/assignment_1/Main.java
@@ -0,0 +1,15 @@
+package hadoop_ece.assignment_1;
+
+import hadoop_ece.assignment_1.practice_1.Practice_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args ) throws Exception
+    {
+    	Practice_1 ptc1 = new Practice_1(args);
+    }
+}
---
+++ src/main/java/hadoop_ece/assignment_1/practice_2/Practice_2.java
@@ -0,0 +1,5 @@
+package hadoop_ece.assignment_1.practice_2;
+
+public class Practice_2 {
+
+}
